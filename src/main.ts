b4fa06171c8d9bf17ca0029d4813b1eae7aa1f47
Arie Milner <amilner42@gmail.com>
2016-03-20 17:45:25 -0400

    Change URL strategy so that the page can be refreshed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy } from 'angular2/router';
 import {AppComponent} from './app/app.component';
 import {StudentService} from './shared/services/student.service';
 import {ColorService} from './shared/services/color.service';
@@ -11,5 +12,6 @@ bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
   StudentService,
   ColorService,
-  provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' })
+  provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' }),
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
