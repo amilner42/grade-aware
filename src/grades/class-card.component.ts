0c91b8a1044ab7b93e14ce3d5f155357c0f6b0bf
Arie Milner <amilner42@gmail.com>
2016-03-19 20:55:42 -0400

    Made default color of class cards grey, added active to the components

--- src/grades/class-card.component.ts
+++ src/grades/class-card.component.ts
@@ -5,6 +5,7 @@ import {Injectable} from 'angular2/core';
 import {ColorService} from '../shared/services/color.service';
 import {Inject} from 'angular2/core';
 import {StudentService} from '../shared/services/student.service';
+import {NgClass} from 'angular2/common';
 
 @Component({
   moduleId: module.id,
@@ -12,12 +13,16 @@ import {StudentService} from '../shared/services/student.service';
   templateUrl: './class-card.component.html',
   styleUrls: [
     './class-card.component.css'
+  ],
+  directives: [
+    NgClass
   ]
 })
 
 @Injectable()
 export class ClassCard {
 
+  private static allClassCards: ClassCard[] = [];
   /* tslint:disable */
   @Input() private classWithGrades: Class;
   /* tslint:enable */
@@ -25,12 +30,26 @@ export class ClassCard {
   private classAverage: number;
   private ColorService;
   private StudentService;
+  private isActive: boolean;
 
+  // Add this class card to the static list of class cards, inject services.
   constructor(@Inject(ColorService) ColorService, @Inject(StudentService) StudentService) {
+    this.isActive = ClassCard.allClassCards.length === 0;
+    ClassCard.allClassCards.push(this);
     this.ColorService = ColorService;
     this.StudentService = StudentService;
   }
 
+  // Turn all class cards off, and then turn this one on.
+  public setActive() {
+    for(var currentCard of ClassCard.allClassCards) {
+        currentCard.isActive = false;
+    }
+    this.isActive = true;
+  }
+
+  // In ngOnInit you can access inputs to the component, you cannot do this in
+  // the constructor.
   ngOnInit() {
     this.classAverage = this.StudentService.averageGradeForClass(this.classWithGrades);
     this.borderColor = this.ColorService.getColorFromScore(this.classAverage);
